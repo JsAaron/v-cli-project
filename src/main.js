cdba706ffe2fa2db578a00006e764e7c89f5b271
aaron <401545553@qq.com>
2016-08-29 17:06:28 +0800

    no message

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
+const VueRouter = require('vue-router')
+import { configRouter } from './route-config'
 
+Vue.use(VueRouter)
 
-var app = new Vue({
-    el: '#app'
-})
+const router = new VueRouter()
+const app = require('./app.vue')
+
+configRouter(router)
+
+const App = Vue.extend(app)
+
+router.start(App, '#app')
